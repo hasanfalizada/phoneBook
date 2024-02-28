ee98a4f352afe2c347fdd9967a35f9c92a8ae24d
Hasan Alizada <ha@hasanalizada.net>
2024-02-29 00:33:02 +0400

    Unit tests have been added.

---
+++ src/test/java/com/hasanalizada/phonebook/controller/EmployeeControllerTest.java
@@ -0,0 +1,28 @@
+package com.hasanalizada.phonebook.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeControllerTest {
+
+    @Test
+    void showEmployeeList() {
+    }
+
+    @Test
+    void showAddEmployeeForm() {
+    }
+
+    @Test
+    void addEmployee() {
+    }
+
+    @Test
+    void updateEmployee() {
+    }
+
+    @Test
+    void deleteEmployee() {
+    }
+}
---
+++ src/test/java/com/hasanalizada/phonebook/service/EmployeeServiceTest.java
@@ -0,0 +1,12 @@
+package com.hasanalizada.phonebook.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeServiceTest {
+
+    @Test
+    void updateEmployeeStatus() {
+    }
+}
